06cfb69dafaa368344d7e9d05e2ee1a8fa580ce1
Mauricio Jacobo <david.jacobo@utec.edu.pe>
2023-03-08 19:20:12 -0600

    Proyecto terminado

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,16 +1,26 @@
 import React from 'react';
+import '../stylesheets/Testimonio.css'
 
-function Testimonio (){
+function Testimonio (props){
   return(
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
-      src={require('../imagenes/testimonio-emma.png')}
-      alt='Foto de Emma'/>
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt={`Foto de ${props.imagen}`}/>
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'> Emma Bostian en Suecia</p>
-
+        <p className='nombre-testimonio'> 
+          <strong>{props.nombre} </strong>in <strong>{props.pais}</strong>
+        </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className='textyo-testimonio'>
+          "{props.testimonio}"
+        </p>
 
       </div>
     </div>
   )
 }
+
+export default Testimonio;
